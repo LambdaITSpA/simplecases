7f6c5f4735653e7c5b0e9486ff1ddb3f7848b741
Claudio Guerra <claudevandort@gmail.com>
2014-10-21 15:11:27 -0300

    rename .docker to docker

--- docker.sh
+++ docker.sh
@@ -1,10 +1,10 @@
 #sudo echo "127.0.0.1 webapp.dev" >> /etc/hosts
-ln Gemfile .docker/rails/Gemfile
-ln Gemfile.lock .docker/rails/Gemfile.lock
-docker build -t simplecases-dev-pg .docker/pg
-docker build -t simplecases-dev-rails rails .docker/rails/development
-docker build -t simplecases-prod-rails rails .docker/rails/production
-rm .docker/rails/Gemfile .docker/rails/Gemfile.lock
+ln Gemfile docker/rails/Gemfile
+ln Gemfile.lock docker/rails/Gemfile.lock
+docker build -t simplecases-dev-pg docker/pg
+docker build -t simplecases-dev-rails rails docker/rails/development
+#docker build -t simplecases-prod-rails rails docker/rails/production
+rm docker/rails/Gemfile docker/rails/Gemfile.lock
 docker rm -f rails; docker rm -f pg
 docker run -d -p 127.0.0.1:5432:5432 --name pg pg
 docker run -d --net="host" -v $PWD:/var/www/webapp:ro --name rails rails
