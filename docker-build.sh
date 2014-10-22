a3f691677e19ffdc7c6ece8210cfd174077abd44
Claudio Guerra <claudevandort@gmail.com>
2014-10-22 12:02:28 -0300

    modify dockerfile

---
+++ docker-build.sh
@@ -0,0 +1,11 @@
+#sudo echo "127.0.0.1 webapp.dev" >> /etc/hosts
+ln Gemfile docker/rails/development/Gemfile
+ln Gemfile.lock docker/rails/development/Gemfile.lock
+#ln Gemfile docker/rails/production/Gemfile
+#ln Gemfile.lock docker/rails/production/Gemfile.lock
+docker build -t claudevandort/simplecases-pg:dev docker/pg
+docker build -t claudevandort/simplecases-rails:dev docker/rails/development
+#docker build -t claudevandort/simplecases-rails:prod docker/rails/production
+rm docker/rails/development/Gemfile docker/rails/development/Gemfile.lock
+#docker run -d -p 127.0.0.1:80:80 --name rails rails
+#sudo docker run --name rails -i -t rails /bin/bash
