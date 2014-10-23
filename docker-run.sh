78317e6ad2777e34fcc11761fd97fc3999dc284a
Claudio Guerra <claudevandort@gmail.com>
2014-10-23 13:37:20 -0300

    add landing page template

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 docker rm -f simplecases-pg-dev; docker rm -f simplecases-rails-dev
 service apache2 stop; service postgresql stop
 docker run -d -p 127.0.0.1:5432:5432 --name simplecases-pg-dev claudevandort/simplecases-pg:dev
-docker run -d --net="host" -v $PWD:/var/www/webapp:ro --name simplecases-rails-dev claudevandort/simplecases-rails:dev
+docker run -d --net="host" -v $PWD:/var/www/webapp:rw --name simplecases-rails-dev claudevandort/simplecases-rails:dev
 #docker run --net="host" -v $PWD:/var/www/webapp:ro --name simplecases-rails-dev -i -t claudevandort/simplecases-rails:dev /bin/bash
 #docker run --name simplecases-pg-dev -i -t claudevandort/simplecases-pg:dev /bin/bash
