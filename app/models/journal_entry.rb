74e85807dfdcb299c228a82d0c2099e4ed46297a
Patricio <patricioalfredo18@gmail.com>
2014-10-24 14:30:05 -0300

    add relationship journal and users migration

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,3 +1,4 @@
 class JournalEntry < ActiveRecord::Base
   belongs_to :cause_state
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   belongs_to :organization
   has_many :user_causes
+  has_many :journal_entries
 end
