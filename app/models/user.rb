74fc3cce2bd461fab5582bb6c9dce51f7fa5b3dd
Claudio Guerra <claudevandort@gmail.com>
2014-11-01 15:08:14 -0300

    user is not registerable anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable#, :registerable
   belongs_to :organization
   has_many :user_causes
   has_many :journal_entries
