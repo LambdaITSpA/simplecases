9349180638ca5a073a949aef87c80721d36e383a
Claudio Guerra <claudevandort@gmail.com>
2014-11-07 10:56:01 -0300

    modify docker image

--- docker-run-prod.sh
+++ docker-run-prod.sh
@@ -1,6 +1,8 @@
 #docker rm -f simplecases-pg-dev
 docker rm -f simplecases-rails-prod
 service apache2 stop; service postgresql stop
+#rake db:migrate RAILS_ENV=development
+#rake db:seed
 #docker run -d -p 127.0.0.1:5432:5432 --name simplecases-pg-dev claudevandort/simplecases-pg:dev
 docker run -d --net="host" -v $PWD:/var/www/webapp:rw --name simplecases-rails-prod claudevandort/simplecases-rails:prod
 #docker run --net="host" -v $PWD:/var/www/webapp:rw --name simplecases-rails-prod -i -t claudevandort/simplecases-rails:prod /bin/bash
