ec3a9e1e078e58542ac9f1e204c60f5affb9fc3c
Claudio Guerra <claudevandort@gmail.com>
2014-11-14 17:51:47 -0300

    fix some navigation issues

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,4 +1,5 @@
 class JournalEntriesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_journal_entry, only: [:show, :edit, :update, :destroy]
   before_action :set_cause
 
