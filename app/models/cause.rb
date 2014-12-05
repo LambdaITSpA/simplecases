769e2fa26c4ca77b9b08c13e96270ee0a8ae3ddb
Patricio Jara <patricioalfredo18@gmail.com>
2014-12-05 20:30:40 -0300

    modificar cantidad de cuotas

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -13,9 +13,18 @@ class Cause < ActiveRecord::Base
       self.payments << Payment.create(date: self.first_payment_date + quantity.month, payed: false, amount: self.honorary/self.fee_quantity, payment_number: quantity+1)
     end
   end
-  def update_payment_dates
-    self.payments.delete_all
-    set_payment_dates
+  def update_payment_dates 
+    if self.payments.payed.count > 0
+      amount_payable = self.honorary - self.payments.payed.sum('amount')
+      months = self.fee_quantity - self.payments.payed.count
+      self.payments.unpayed.delete_all
+      months.times do |quantity|
+        self.payments << Payment.create(date: self.payments.payed.last.date + (quantity + 1).month, payed: false, amount: amount_payable/months, payment_number:  self.payments.payed.count + quantity+1)
+      end
+    else
+      self.payments.delete_all
+      set_payment_dates
+    end
   end
 
   def journal_entries
