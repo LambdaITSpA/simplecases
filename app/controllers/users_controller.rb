2bbe927fd77aff61a1b3bf0d1eb4d32223c2b798
Claudio Guerra <claudevandort@gmail.com>
2014-12-22 00:10:16 -0300

    update user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -83,6 +83,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :id_number, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :id_number, :email, :password, :password_confirmation, :user_type_id)
     end
 end
