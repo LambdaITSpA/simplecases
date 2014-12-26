96bd99f0ec93f6759295e31941bf23d622bc04ee
Claudio Guerra <claudevandort@gmail.com>
2014-12-26 18:52:55 -0300

    daily email working with whenever gem

--- docker-run.sh
+++ docker-run.sh
@@ -1,8 +1,9 @@
-docker rm -f simplecases-pg-dev; docker rm -f simplecases-rails-dev
+docker rm -f simplecases-pg-dev; docker rm -f simplecases-rails-dev; docker rm -f simplecases-rails-cron-dev
 service apache2 stop; service postgresql stop
 docker run -d -p 127.0.0.1:5432:5432 --name simplecases-pg-dev claudevandort/simplecases-pg:dev
 /bin/bash -l -c "rake db:migrate RAILS_ENV=development"
 /bin/bash -l -c "rake db:seed --trace"
 docker run -d --net="host" -v $PWD:/var/www/webapp:rw --name simplecases-rails-dev claudevandort/simplecases-rails:dev
+docker run -d --net="host" -v $PWD:/var/www/webapp:rw --name simplecases-rails-cron-dev claudevandort/simplecases-rails:dev /bin/bash -l -c "service postfix restart; whenever --update-crontab; cron -f"
 #docker run --net="host" -v $PWD:/var/www/webapp:rw --name simplecases-rails-dev -i -t claudevandort/simplecases-rails:dev /bin/bash
 #docker run --name simplecases-pg-dev -i -t claudevandort/simplecases-pg:dev /bin/bash
