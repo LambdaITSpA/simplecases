e1ae75ce7e9a1a1c804eb1d5556865d70e8207f3
Patricio Jara <patricioalfredo18@gmail.com>
2015-02-18 14:53:07 -0300

    asd

---
+++ load.rb
@@ -0,0 +1,18 @@
+supr_apel = Area.find(7)
+
+	Court.create [{name: 'C.A Antofagasta', area: supr_apel},
+			{name: 'C.A Arica', area: supr_apel},
+			{name: 'C.A Chillan', area: supr_apel},
+			{name: 'C.A Concepción', area: supr_apel},
+			{name: 'C.A Copiapó', area: supr_apel},
+			{name: 'C.A Coyhaique', area: supr_apel},
+			{name: 'C.A Iquique', area: supr_apel},
+			{name: 'C.A La Serena', area: supr_apel},
+			{name: 'C.A De Prueba', area: supr_apel},
+			{name: 'C.A Puerto Montt', area: supr_apel},
+			{name: 'C.A Punta Arenas', area: supr_apel},
+			{name: 'C.A Rancagua', area: supr_apel},
+			{name: 'C.A Talca', area: supr_apel},
+			{name: 'C.A Temuco', area: supr_apel},
+			{name: 'C.A Valdivia', area: supr_apel},
+			{name: 'C.A Valparaíso', area: supr_apel}]
